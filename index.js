e1c8cec108e0f7381dc8f6bc8ea42aa39c11fdc2
Mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-06-08 17:18:05 +0530

    Add to dataset

---
+++ index.js
@@ -0,0 +1,17 @@
+const DATASETS = {
+    videogames: {
+      TITLE: "Video Game Sales",
+      DESCRIPTION: "Top 100 Most Sold Video Games Grouped by Platform",
+      FILE_PATH: "https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/video-game-sales-data.json" },
+  
+    movies: {
+      TITLE: "Movie Sales",
+      DESCRIPTION: "Top 100 Highest Grossing Movies Grouped By Genre",
+      FILE_PATH: "https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/movie-data.json" },
+  
+    kickstarter: {
+      TITLE: "Kickstarter Pledges",
+      DESCRIPTION: "Top 100 Most Pledged Kickstarter Campaigns Grouped By Category",
+      FILE_PATH: "https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/kickstarter-funding-data.json" } };
+  
+  
